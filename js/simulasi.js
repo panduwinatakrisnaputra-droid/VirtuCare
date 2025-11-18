e06da48234611b5ae6daf4c1e475bd9d1e1faa89
Telo Ryjeng <146211829+teloryjeng@users.noreply.github.com>
2025-11-18 10:08:13 +0700

    Update simulasi.js

--- js/simulasi.js
+++ js/simulasi.js
@@ -639,7 +639,7 @@ function resetItem(mesh, initialPosition, initialRotation) {
     dialogBody = new BABYLON.GUI.TextBlock();
     dialogBody.color = "white";
     dialogBody.fontSizeInPixels = 70;
-    dialogBody.heightInPixels = 700;
+    dialogBody.heightInPixels = 650;
     dialogBody.textWrapping = true;
     stackPanel.addControl(dialogBody);
 
@@ -751,6 +751,7 @@ createScene().then(scene => {
 });
 
 window.addEventListener("resize", () => engine.resize());
+
 
 
 
