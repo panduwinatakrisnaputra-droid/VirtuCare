79f397ca9f3934cdf3bf5c960505bc8c88347d1e
panduwinatakrisnaputra-droid <panduwinatakrisnaputra@gmail.com>
2025-11-27 13:44:01 +0700

    Update simulasi.js

--- js/simulasi.js
+++ js/simulasi.js
@@ -351,7 +351,7 @@ const createScene = async function () {
      
     // Invisible interaction points
     const chestTarget = BABYLON.MeshBuilder.CreateSphere("tChest", { diameter: 0.5 }, scene);
-    chestTarget.position = new BABYLON.Vector3(-14.6, 1.4, 27);
+    chestTarget.position = new BABYLON.Vector3(-14.6, 1.35, 27);
     chestTarget.isVisible = false; // Set ke false agar tidak terlihat
 
     const headTarget = BABYLON.MeshBuilder.CreateSphere("tHead", { diameter: 0.5 }, scene);
@@ -1494,5 +1494,6 @@ createScene().then(scene => {
 });
 
 window.addEventListener("resize", () => engine.resize());
+
 
 
